831c6194a9577e62e1b82468fed487fee6cee7e9
Im-perfect <zhangke.kerri@gmail.com>
2019-10-17 13:46:00 +0200

    add event router and model

---
+++ event/router.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+const router = new Router()
+const Event = require('./model')
+
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express()
+const eventRouter = require('./event/router')
+
+
+app.use(eventRouter)
 
 app.listen(4000, ()=>console.log('listening on 4000'))
