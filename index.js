80d478419722ece196db1851cf2dceee4922431e
Im-perfect <zhangke.kerri@gmail.com>
2019-10-17 13:57:45 +0200

    add body parser, cors middlewares

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+
 const eventRouter = require('./event/router')
 
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+const bodyParser = require('body-parser')
+const bodyParserMiddleware = bodyParser.json()
+app.use(bodyParserMiddleware)
 
 app.use(eventRouter)
 
-app.listen(4000, ()=>console.log('listening on 4000'))
+app.listen(process.env.PORT || 4000, ()=>console.log('listening on 4000'))
